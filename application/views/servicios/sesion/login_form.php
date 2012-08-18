9d2d93728324b40bd245561fd892c81f9dfee8fe
Saúl Espinosa <rinku.enlace@gmail.com>
2012-08-18 16:31:54 -0500

    Vista de el formulario de login

---
+++ application/views/servicios/sesion/login_form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Login</title>
+</head>
+
+<body>
+	<?=validation_errors()?>
+	<?=form_open('servicios/sesion/login');?>
+		<fieldset>
+			<label for="usuario">Correo electrónico:</label>
+			<input type="text" name="usuario" id="usuario"/>
+			<label for="usuario">Contraseña:</label>
+			<input type="password" name="contrasenia" id="contrasenia"/>
+			<input type="submit" value="Iniciar sesión"/>
+		</fieldset>
+	</form>
+	
+</body>
+</html>

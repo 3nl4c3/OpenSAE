6fde8b5e5f0a3480253b4995f163e793a74b2713
Saul Espinosa <rinku.enlace@gmail.com>
2013-12-01 11:41:40 -0600

    Nuevos metodos
    Dos nuevos metedos para implemenrtar

---
+++ application/controllers/administrador/usuario.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Usuario extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+	/**
+	 * metodo que da de alta a un usuario, se da por medio de post.
+	 * @return void
+	 */
+	public function alta()
+	{
+
+	}
+
+	/**
+	 * pone la flag de desactivacion, por si el usuario ya no puede tener acceso.
+	 * @return [type]
+	 */
+	public function desactivar()
+	{
+
+	}
+
+}
+
+/* End of file usuario.php */
+/* Location: ./application/controllers/administrador/usuario.php */
